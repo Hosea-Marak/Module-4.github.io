faf0b9c2af14c2fbc1d6535f683345cfd223a2c9
Hosea-Marak <hoseamk3g@gmail.com>
2020-06-19 18:26:57 +0530

    Newly commited

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,13 @@
 (function(){
     var names =["Hosea","Tangrime","John","Emily","Sam","Bob","Paul","Jacob","Paulo","Eric","Cena","disco","dj khalid","john cena","jimmy"];
-    // for(var i=0;i<names.length;i++){
-    //     var firstletter = names[i].charAt(0).toLowerCase();
-    //     if(firstletter === 'j'){
-    //         byespeak.speak(names[i]);
-    //     }
-    //     else{
-    //        hellospeak.speak(names[i]);
-    //     }
-    // }
-    for (name in names){
-        name = names[name];
-        if(name[0].toLowerCase()=='j'){
-            byespeak.speak(name);
+    for(var i=0;i<names.length;i++){
+        var firstletter = names[i].charAt(0).toLowerCase();
+        if(firstletter === 'j'){
+            byespeak.speak(names[i]);
         }
         else{
-            hellospeak.speak(name);
+           hellospeak.speak(names[i]);
         }
     }
+
 })();
